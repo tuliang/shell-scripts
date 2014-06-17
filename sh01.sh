d01046c6d3009b761d40aa1b175bda78c0967cce
tuliang <info@tuliang.org>
2014-06-17 12:26:27 +0800

    fix path

--- sh01.sh
+++ sh01.sh
@@ -2,9 +2,14 @@
 
 echo "Hello!"
 
+export PATH=$PATH:/opt/rvm/bin:/opt/rvm/sbin
+
 \curl -sSL https://get.rvm.io | bash -s stable
 
-source ~/.rvm/scripts/rvm
+source ~/.bashrc
+source ~/.bash_profile
+
+sed -i -e 's/ftp\.ruby-lang\.org\/pub\/ruby/ruby\.taobao\.org\/mirrors\/ruby/g' ~/.rvm/config/db
 
 rvm install 2.1.1
 
