9e681dfe4a6ec2500b0193e39ff61635f8561370
tuliang <info@tuliang.org>
2014-06-17 11:54:43 +0800

    fix rvm gem ruby and rails

--- sh01.sh
+++ sh01.sh
@@ -4,8 +4,16 @@ echo "Hello!"
 
 \curl -sSL https://get.rvm.io | bash -s stable
 
-source /etc/profile.d/rvm.sh
+source ~/.rvm/scripts/rvm
 
 rvm install 2.1.1
+
+ruby -v
+
+gem -v
+
+gem install rails
+
+rails -v
 
 exit 0
