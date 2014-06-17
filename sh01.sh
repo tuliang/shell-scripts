5970abaa7683611698b7fe3f567c84bcec4307f3
tuliang <info@tuliang.org>
2014-06-17 15:16:48 +0800

    fix path

--- sh01.sh
+++ sh01.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo "Hello!"
-
+echo "Install RVM"
 \curl -sSL https://get.rvm.io | bash -s stable
 
-echo 'PATH=$PATH:/opt/rvm/bin:/opt/rvm/sbin' >> ~/.bashrc
-echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bashrc
+# 环境变量
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >>~/.bashrc
+source ~/.bashrc
 
+# ruby更改为taobao源
 sed -i 's!cache.ruby-lang.org/pub/ruby!ruby.taobao.org/mirrors/ruby!' $rvm_path/config/db
 
+# 安装ruby
 rvm install 2.1.1
 rvm use 2.1.1 --default
 
@@ -16,10 +18,12 @@ ruby -v
 
 gem -v
 
+# gem更改为taobao源
 gem sources --remove https://rubygems.org/
 gem sources -a https://ruby.taobao.org/
 gem sources -l
 
+# 安装rails
 gem install rails
 
 rails -v
